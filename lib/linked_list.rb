dd2d5fc7f911c9a3aa781eb95d3a7cceb702ab20
KellyMark <kmark602@gmail.com>
2023-11-08 15:40:21 -0500

    commit tree af09be74745f70642f5c39def07fecd6d206d5cd

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,26 @@ require_relative './node'
 
 class LinkedList
 
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend(new_head)
+        old_head = @head
+        @head = new_head
+        @head.next_node = old_head
+    end
+
+    def append
+
+    end
+
+    def add_after
+
+    end
+
+
+
 end
